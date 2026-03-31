88b85462355f68fa3c209b832d23e2e443129e58
Amfibiy <syropyatov.arseniy@gmail.com>
2026-03-31 20:38:45 +0500

    fix: remove redundant admin_handlers imports from init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,7 +3,6 @@ from .collection import register_collection_handlers
 from .list_handlers import register_list_handlers
 from .clean_handlers import register_clean_handlers
 from .callbacks import register_callbacks
-from .admin_handlers import register_admin_handlers
 
 def register_all_handlers(bot, active_collections, test_collection, known_groups, user_sessions):
     
@@ -16,4 +15,3 @@ def register_all_handlers(bot, active_collections, test_collection, known_groups
     register_clean_handlers(bot, active_collections, test_collection, known_groups, user_sessions)
     
     register_callbacks(bot, active_collections, test_collection, known_groups, user_sessions)
-    register_admin_handlers(bot, known_groups)
